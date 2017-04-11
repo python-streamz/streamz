82ce4ff44a9318d1a6f032670fec54256ae1fc83
Matthew Rocklin <mrocklin@gmail.com>
2017-04-11 15:46:40 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup
+
+
+setup(name='streams',
+      version='0.0.1',
+      description='Streams',
+      url='http://github.com/mrocklin/streams/',
+      maintainer='Matthew Rocklin',
+      maintainer_email='mrocklin@gmail.com',
+      license='BSD',
+      keywords='streams,dask',
+      packages=['streams'],
+      long_description=(open('README.rst').read() if exists('README.rst')
+                        else ''),
+      install_requires=list(open('requirements.txt').read().strip().split('\n')),
+      zip_safe=False)
