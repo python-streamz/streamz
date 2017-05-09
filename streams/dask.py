f0b1691961303bf3100579ac34207e0b0393f64a
Matthew Rocklin <mrocklin@gmail.com>
2017-05-09 13:47:49 -0400

    Replace DaskStream with monkey patching on Future objects

--- streams/dask.py
+++ streams/dask.py
@@ -1,4 +1,4 @@
-from distributed.client import default_client
+from distributed.client import default_client, Future
 from tornado.locks import Condition
 from tornado.queues import Queue
 from tornado import gen
@@ -7,55 +7,19 @@ from . import core
 from .core import Stream
 
 
-class DaskStream(Stream):
-    """ A Stream of Dask futures
-
-    This follows a subset of the Stream API but operates on a cluster
-
-    Examples
-    --------
-    >>> from dask.distributed import Client
-    >>> client = Client()
-    >>> source = Stream()
-    >>> s = source.to_dask().map(func).gather()
-    """
-    def map(self, func, **kwargs):
-        """ Apply a function on every element of the stream with Dask
-
-        This uses ``Client.submit`` to call a function on the element
-        asynchronously on a Dask cluster.  It returns a stream of futures.
-        """
-        return map(func, self, **kwargs)
-
-    def scan(self, func, start=core.no_default):
-        """ Accumulate results with previous state
-
-        This uses ``Client.submit`` to call the function using a Dask client.
-        It returns a stream of futures.
-        """
-        return scan(func, self, start=start)
-
-    accumulate = scan
-
-    def scatter(self, limit=10, client=None):
-        """ Scatter data out to a cluster
-
-        This returns a stream of futures
-        """
-        return scatter(self, limit=limit, client=client)
-
-    def gather(self, limit=10, client=None):
-        """ Gather a stream of futures back to a stream of local results """
-        return gather(self, limit=limit, client=client)
-
-    def update(self, x, who=None):
-        return self.emit(x)
-
-    def zip(self, other):
-        return zip(self, other)
+def __stream_map__(self, func):
+    return default_client().submit(func, self)
 
 
-class scatter(DaskStream):
+def __stream_reduce__(self, func, accumulator):
+    return default_client().submit(func, accumulator, self)
+
+
+Future.__stream_map__ = __stream_map__
+Future.__stream_reduce__ = __stream_reduce__
+
+
+class scatter(Stream):
     def __init__(self, child, limit=10, client=None):
         self.client = client or default_client()
         self.queue = Queue(maxsize=limit)
@@ -117,34 +81,3 @@ class gather(Stream):
     def flush(self):
         while not self.queue.empty():
             yield self.condition.wait()
-
-
-class map(DaskStream):
-    def __init__(self, func, child, client=None, **kwargs):
-        self.client = client or default_client()
-        self.func = func
-        self.kwargs = kwargs
-
-        Stream.__init__(self, child)
-
-    def update(self, x, who=None):
-        return self.emit(self.client.submit(self.func, x, **self.kwargs))
-
-
-class scan(DaskStream):
-    def __init__(self, func, child, start=core.no_default, client=None):
-        self.client = client or default_client()
-        self.func = func
-        self.state = start
-
-        Stream.__init__(self, child)
-
-    def update(self, x, who=None):
-        if self.state is core.no_default:
-            self.state = x
-        else:
-            self.state = self.client.submit(self.func, self.state, x)
-            return self.emit(self.state)
-
-class zip(core.zip, DaskStream):
-    pass
