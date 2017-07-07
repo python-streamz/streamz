3068b43ae5aecd0cca0a8001a25b0006b20146cd
Christopher J. Wright <cjwright4242gh@gmail.com>
2017-07-07 11:26:48 -0400

    Coverage (#30)
    * CI: add coverage
    
    * CI: use conda-forge
    
    * CI: coverage run
    
    * CI: run run_tests

---
+++ run_tests.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+import pytest
+import os
+
+if __name__ == '__main__':
+    # show output results from every test function
+    args = ['-v']
+    # show the message output for skipped and expected failure tests
+    if len(sys.argv) > 1:
+        args.extend(sys.argv[1:])
+    print('pytest arguments: {}'.format(args))
+    # # compute coverage stats for xpdAcq
+    # args.extend(['--cov', 'xpdAcq'])
+    # call pytest and exit with the return code from pytest so that
+    # travis will fail correctly if tests fail
+    exit_res = pytest.main(args)
+    sys.exit(exit_res)
