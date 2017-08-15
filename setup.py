ff329252b62b68a89eeccd16faa3ad98a52f2a52
Matthew Rocklin <mrocklin@gmail.com>
2017-08-15 18:44:18 -0400

    Releasee version 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 setup(name='streamz',
-      version='0.0.1',
+      version='0.1.0',
       description='Streams',
       url='http://github.com/mrocklin/streamz/',
       maintainer='Matthew Rocklin',
