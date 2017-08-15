2b6e11128b912c5eec6ecc587556f68c7bce9dab
christopher <cjwright4242@gmail.com>
2017-08-15 09:56:01 -0400

    FIX: fix setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='dask-streams',
       maintainer_email='mrocklin@gmail.com',
       license='BSD',
       keywords='streams',
-      packages=['streams'],
+      packages=['dask_streams'],
       long_description=(open('README.rst').read() if exists('README.rst')
                         else ''),
       install_requires=list(open('requirements.txt').read().strip().split('\n')),
