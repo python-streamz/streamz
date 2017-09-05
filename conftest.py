a9bdc6c509a6f77c6df7a6a59dbeaea22a79c8a4
Matthew Rocklin <mrocklin@gmail.com>
2017-09-05 14:59:12 -0400

    Mark slow tests as slow
    Also extend timeout in test_buffer_sync

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption("--runslow", action="store_true", help="run slow tests")
+
+
+def pytest_runtest_setup(item):
+    if 'slow' in item.keywords and not item.config.getoption("--runslow"):
+        pytest.skip("need --runslow option to run")
+
+
+def pytest_ignore_collect(path, config):
+    if 'run_test.py' in str(path):
+        return True
