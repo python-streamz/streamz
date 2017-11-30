c55ad8dd3df306ec4625441c5c93f7b74ef58dc9
Tobias Brandt <Tobias.Brandt@gmail.com>
2017-11-30 23:23:28 +0200

    BUG: timed_window produces AttributeError with asyncio event loop

--- examples/fib_asyncio.py
+++ examples/fib_asyncio.py
@@ -9,7 +9,7 @@ s = source.sliding_window(2).map(sum)
 L = s.sink_to_list()                    # store result in a list
 
 s.rate_limit(0.5).sink(source.emit)         # pipe output back to input
-s.rate_limit(1.0).sink(lambda x: print(L))  # print state of L every second
+s.timed_window(1.0).sink(lambda x: print(x))
 
 source.emit(0)                          # seed with initial values
 source.emit(1)
