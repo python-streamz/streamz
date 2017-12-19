b09da132015153af71860787eaa6537c464183cf
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2017-12-19 16:29:21 +0000

    Fixed flakes in holoviews tests

--- streamz/dataframe/tests/test_holoviews.py
+++ streamz/dataframe/tests/test_holoviews.py
@@ -1,14 +1,11 @@
 from __future__ import division, print_function
 
 from holoviews.core import NdOverlay, Dimension
-from holoviews.element import (
-    Curve, Scatter, Area, Bars, BoxWhisker, Dataset, Distribution,
-    Table
-)
+from holoviews.element import Curve, Scatter, Area, Bars
 from holoviews.streams import Buffer, Pipe
 import pytest
 import streamz.dataframe as sd
-import streamz.dataframe.holoviews
+import streamz.dataframe.holoviews # flake8: noqa
 
 ELEMENT_TYPES = {'line': Curve, 'scatter': Scatter, 'area': Area,
                  'bars': Bars, 'barh': Bars}
@@ -19,20 +16,24 @@ def test_sdf_stream_setup():
     dmap = source.plot()
     assert isinstance(dmap.streams[0], Buffer)
 
+
 def test_series_stream_setup():
     source = sd.Random(freq='10ms', interval='100ms')
     dmap = source.x.plot()
     assert isinstance(dmap.streams[0], Buffer)
+
 
 def test_ssdf_stream_setup():
     source = sd.Random(freq='10ms', interval='100ms')
     dmap = source.groupby('y').sum().plot()
     assert type(dmap.streams[0]) is Pipe
 
+
 def test_sseries_stream_setup():
     source = sd.Random(freq='10ms', interval='100ms')
     dmap = source.groupby('y').sum().x.plot()
     assert type(dmap.streams[0]) is Pipe
+
 
 @pytest.mark.parametrize('kind', ['line', 'scatter', 'area'])
 def test_plot_multi_column_chart_from_sdf(kind):
@@ -44,6 +45,7 @@ def test_plot_multi_column_chart_from_sdf(kind):
     assert isinstance(element.last, ELEMENT_TYPES[kind])
     assert element.last.kdims == [Dimension('index')]
 
+
 @pytest.mark.parametrize('kind', ['line', 'scatter', 'area', 'bars', 'barh'])
 def test_plot_xy_chart_from_sdf(kind):
     source = sd.Random(freq='10ms', interval='100ms')
@@ -52,6 +54,7 @@ def test_plot_xy_chart_from_sdf(kind):
     assert isinstance(element, ELEMENT_TYPES[kind])
     assert element.kdims == [Dimension('index')]
     assert element.vdims == [Dimension('y')]
+
 
 @pytest.mark.parametrize('kind', ['line', 'scatter', 'area', 'bars', 'barh'])
 def test_plot_xy_chart_from_series(kind):
