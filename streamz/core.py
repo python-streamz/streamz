d095650a96871619da86156d177ba9826e49bcaf
Christopher J. Wright <cjwright4242@gmail.com>
2019-05-07 13:07:10 -0400

    remove unused import

--- streamz/core.py
+++ streamz/core.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
-from collections import deque, Hashable
+from collections import deque
 from datetime import timedelta
 import functools
 import logging
