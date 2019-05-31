ba3c16435a8059d4d2b83cc7e8eeeff3dafe2285
Christopher J. Wright <cjwright4242@gmail.com>
2019-05-31 16:26:54 -0400

    py3k only

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(name='streamz',
       license='BSD',
       keywords='streams',
       packages=packages + tests,
+      python_requires='>=3.5',
       long_description=(open('README.rst').read() if exists('README.rst')
                         else ''),
       install_requires=list(open('requirements.txt').read().strip().split('\n')),
