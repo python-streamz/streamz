542c8874ddb6ce868cf7d021650f2d8887eb5107
Chinmay Chandak <cchandak@nvidia.com>
2020-07-16 17:57:20 +0000

    Tags for 0.5.4 release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ tests = [p + '.tests' for p in packages]
 
 
 setup(name='streamz',
-      version='0.5.3',
+      version='0.5.4',
       description='Streams',
       url='http://github.com/python-streamz/streamz/',
       maintainer='Matthew Rocklin',
