5ad905ef86e6857fc6a49aaffa857f50951dc418
David Skoog <dwskoog@bastille.io>
2025-12-23 09:40:32 -0500

    Use Stream.connect to loop the fib examples

--- examples/fib_thread.py
+++ examples/fib_thread.py
@@ -4,10 +4,10 @@ source = Stream()
 s = source.sliding_window(2).map(sum)
 L = s.sink_to_list()                         # store result in a list
 
-s.rate_limit('500ms').sink(source.emit)      # pipe output back to input
 s.rate_limit('1s').sink(lambda x: print(L))  # print state of L every second
+s.rate_limit('500ms').connect(source)        # pipe output back to input
 
 try:
-    source.emit(1)                           # seed with initial values, blocks thread due to cycle in stream
+    source.emit(1)                           # seed with initial value, blocks thread due to cycle in stream
 except KeyboardInterrupt:
     pass
--- examples/fib_tornado.py
+++ examples/fib_tornado.py
@@ -6,10 +6,10 @@ source = Stream(asynchronous=True)
 s = source.sliding_window(2).map(sum)
 L = s.sink_to_list()                         # store result in a list
 
-s.rate_limit('500ms').sink(source.emit)      # pipe output back to input
 s.rate_limit('1s').sink(lambda x: print(L))  # print state of L every second
+s.rate_limit('500ms').connect(source)        # pipe output back to input
 
-source.emit(1)                               # seed with initial values, does not block thread due to Future return
+source.emit(1)                               # seed with initial value, does not block thread due to Future return
 
 try:
     IOLoop.current().start()
